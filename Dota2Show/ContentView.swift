e8bea6303804bbdc00166bf8de2771593523ddcc
chr1s78 <ayahirano2014@126.com>
2021-06-27 15:38:38 +0800

    Initial Commit

---
+++ Dota2Show/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dota2Show
+//
+//  Created by 吕博 on 2021/6/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dota2Show/Dota2ShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dota2ShowApp.swift
+//  Dota2Show
+//
+//  Created by 吕博 on 2021/6/27.
+//
+
+import SwiftUI
+
+@main
+struct Dota2ShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
